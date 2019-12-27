4960f100cf16fc933c2603a1e18af92789fc476a
Leon <leon.id@qq.com>
2019-12-27 15:36:56 +0800

    修复ToolWindowCommand.png；修改菜单名称；

--- VSIXProjectDemo/ToolWindows/MainToolWindow.cs
+++ VSIXProjectDemo/ToolWindows/MainToolWindow.cs
@@ -23,7 +23,7 @@
         /// </summary>
         public MainToolWindow() : base(null)
         {
-            this.Caption = "MainToolWindow";
+            this.Caption = "Leon 工具箱";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
